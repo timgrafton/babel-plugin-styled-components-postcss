9f797108133ea73530bbb9347ceb02498bf52c8c
Daniel P. Shannon <daniel@daniel.sh>
2018-07-01 01:57:40 -0400

    Inject complete CSS rules, where possible.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+const { addNamed } = require('@babel/helper-module-imports')
+const deasyncPromise = require('deasync-promise')
 const postcss = require('postcss')
 const postcssrc = require('postcss-load-config')
-const deasyncPromise = require('deasync-promise')
 
 // styled component expression regex
 const expressionsRegex = /__QUASI_EXPR_(\d+)__/g
@@ -38,6 +39,8 @@ const buildQuasisAst = (t, terms) =>
 
 const generateExpressionPlaceholder = i => `__QUASI_EXPR_${i}__`
 
+let injectGlobalImport
+
 module.exports = ({ types: t }) => ({
   visitor: {
     TaggedTemplateExpression(path) {
@@ -47,18 +50,20 @@ module.exports = ({ types: t }) => ({
           quasi: { quasis, expressions },
         },
       } = path
+
       const tmatch = () =>
         (tag.object && tag.object.name && tag.object.name === 'styled') ||
         (tag.property && tag.property.name && tag.property.name === 'extend') ||
         (tag.callee && tag.callee.name && tag.callee.name === 'styled') ||
-        (tag.callee &&
-          tag.callee.object &&
-          tag.callee.object.object &&
-          tag.callee.object.object.name &&
-          tag.callee.object.object.name === 'styled') ||
         (tag.name && tag.name === 'injectGlobal')
 
-      if (!tmatch()) return
+      const hasConfig = tag.callee &&
+        tag.callee.object &&
+        tag.callee.object.object &&
+        tag.callee.object.object.name &&
+        tag.callee.object.object.name === 'styled'
+
+      if (!tmatch() && !hasConfig) return
 
       // extracts css from template literal
       let css = quasis.reduce((acc, { value: { raw } }, i) => {
@@ -66,9 +71,41 @@ module.exports = ({ types: t }) => ({
         return `${acc}${raw}${expr}`
       }, '')
 
-      // const processed = postcss(plugins).process(css, options).css
       const { plugins, options } = deasyncPromise(postcssrc())
-      ;({ css } = deasyncPromise(postcss(plugins).process(css, options)))
+
+      if (hasConfig && expressions.length === 0) {
+        const componentIdProp = tag.arguments[0].properties.find(prop => prop.key.name === 'componentId')
+        const displayNameProp = tag.arguments[0].properties.find(prop => prop.key.name === 'displayName')
+
+        if (componentIdProp && displayNameProp) {
+          const componentId = componentIdProp.value.value
+          const displayName = displayNameProp.value.value
+          const className = `${displayName}-${componentId}`
+
+          css = `.${className}{${css}}`
+          ;({ css } = deasyncPromise(postcss(plugins).process(css, options)))
+
+          injectGlobalImport = injectGlobalImport || addNamed(path, 'injectGlobal', 'styled-components')
+
+          path.parentPath.parentPath.insertAfter(
+            t.expressionStatement(
+              t.taggedTemplateExpression(
+                t.identifier(injectGlobalImport.name),
+                t.templateLiteral(
+                  [t.templateElement({ cooked: css, raw: css })],
+                  [],
+                ),
+              ),
+            ),
+          )
+
+          path.get('quasi').replaceWith(t.templateLiteral([t.templateElement({ cooked: '', raw: '' })], []))
+
+          return
+        }
+      }
+
+      ({ css } = deasyncPromise(postcss(plugins).process(css, options)))
 
       const { quasiTerms, expressionTerms } = splitExpressions(css)
       const quasisAst = buildQuasisAst(t, quasiTerms)
